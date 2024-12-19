9cca5277e657e3e212f7802ddec5fd1ede83de9e
23657 <zhangyonghao1313@gmail.com>
2024-12-19 18:33:36 +0800

    前期工作

---
+++ backend/src/test/java/com/Museum/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Museum.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
